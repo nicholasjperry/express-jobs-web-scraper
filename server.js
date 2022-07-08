6c0d94c56f0f7643190874e0eda9fed3a217b74f
Nick Perry <nicholasjperry1@gmail.com>
2022-07-08 00:29:36 -0400

    Moved data functional expression within the scrape() function

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ require('./utils/scrape');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configuring database
-// const jobRouter = require('./routes/jobRoutes');
-// app.use(jobRouter);
-
 // Express app listening on port 8000
 app.listen(PORT, () => console.log(`The server is running on PORT: ${PORT}`));
 
--- utils/scrape.js
+++ utils/scrape.js
@@ -1,40 +1,35 @@
 const puppeteer = require('puppeteer');
 const db = require('../db');
 const Job = require('../models/job');
-// const express = require('express');
-// const app = express();
-// const jobRouter = require('../routes/jobRoutes');
-
-// app.use(jobRouter);
 
 (async () => {
     try {
         const browser = await puppeteer.launch({
             headless: false,
             defaultViewport: null,
-            args: ['--no-zygote', '--no-sandbox']
+            // args: ['--no-zygote', '--no-sandbox']
         });
-        const url = 'https://www.linkedin.com/jobs';
+        const url = 'https://www.linkedin.com/jobs/search?keywords=Junior%20Software%20Developer&location=Indianapolis%2C%20IN&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0';
 
         // Open browser instance
-        const page = await browser.newPage({ waitUntil: 'domcontentloaded' });
+        const page = await browser.newPage({ waitUntil: 'networkidle0' });
         console.log(`Navigating to ${url}`);
         await page.goto(url);
 
         // Perform job search
-        await page.type('input[name="keywords"]', 'Junior Software Developer');
-        await page.keyboard.press('Tab');
-        await page.keyboard.press('Tab');
+        // await page.type('input[name="keywords"]', 'Junior Software Developer');
+        // await page.keyboard.press('Tab');
+        // await page.keyboard.press('Tab');
 
         // Delete pre-inserted location text
-        const locationInputValue = await page.$eval('input[name="location"]', el => el.value)
-        for(let i=0; i<locationInputValue.length; i++) {
-            await page.keyboard.press('Backspace');
-        }
-        await page.type('input[name="location"]', 'Indianapolis, IN');
-        await page.evaluate(() => document.querySelector('button[data-tracking-control-name="homepage-jobseeker_search-jobs-search-btn"]').click());
-        await page.waitForNavigation();
-        console.log('New page url: ', page.url());
+        // const locationInputValue = await page.$eval('input[name="location"]', el => el.value)
+        // for(let i=0; i<locationInputValue.length; i++) {
+        //     await page.keyboard.press('Backspace');
+        // }
+        // await page.type('input[name="location"]', 'Indianapolis, IN');
+        // await page.evaluate(() => document.querySelector('button[data-tracking-control-name="homepage-jobseeker_search-jobs-search-btn"]').click());
+        // await page.waitForNavigation();
+        // console.log('New page url: ', page.url());
 
         // Scroll to bottom of page, click on 'See More Jobs' and repeat   
         let lastHeight = await page.evaluate('document.body.scrollHeight');
@@ -49,43 +44,45 @@ const Job = require('../models/job');
                 lastHeight = newHeight;
                 seeMoreJobs();
             }
-            Job.insertMany(data)
-                .then(() => {
-                    console.log('Save successful.')
-                })
-                .then(() => {
-                    console.log(data);
-                    page.close();
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-        }
-        scroll(); 
 
-        // Click on 'See More Jobs'
+            // Scrape all junior job titles
+            const data = await page.evaluate(() => {
+                const allJobsArr = Array.from(document.querySelectorAll('a[data-tracking-control-name="public_jobs_jserp-result_search-card"]'));
+                const namesAndUrls = allJobsArr.map(job => {
+                    return {
+                        name: job.innerText,
+                        url: job.href,
+                        path: job.pathname
+                    }
+                });
+                return namesAndUrls;
+                // const juniorJobs = namesAndUrls.filter(function(job) {
+                //     return job.name.includes('Junior') || job.name.includes('Jr') || job.name.includes('Entry') && job.url && job.path;
+                // });
+                // return juniorJobs;
+            });
+            // Job.insertMany(data)
+            //     .then(() => {
+            //         console.log(data);
+            //     })
+            //     .then(() => {
+            //         console.log('Save successful.')
+            //         page.close();
+            //     })
+            //     .catch(err => {
+            //         console.log(err)
+            //     })
+            // Click on 'See More Jobs'
+            console.log(data);
+        }
+        
         const seeMoreJobs = async () => {
-            await page.evaluate(async() => {
+            await page.evaluate(() => {
                 document.querySelector('button[data-tracking-control-name="infinite-scroller_show-more"]').click();
             });
         }
-
-        // Scrape all junior job titles
-        const data = await page.evaluate(() => {
-            const allJobsArr = Array.from(document.querySelectorAll('a[data-tracking-control-name="public_jobs_jserp-result_search-card"]'));
-            const namesAndUrls = allJobsArr.map(job => {
-                return {
-                    name: job.innerText,
-                    url: job.href,
-                    path: job.pathname
-                }
-            });
-            const juniorJobs = namesAndUrls.filter(function(job) {
-                return job.name.includes('Junior') || job.name.includes('Jr') || job.name.includes('Entry') && job.url && job.path;
-            });
-            return juniorJobs;
-        });
+        scroll(); 
     } catch(err) {
         console.log(err);
     }
-})(); 
+})();
