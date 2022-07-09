5412c60b93660f008c352499452269e4c60ce91e
Nick Perry <nicholasjperry1@gmail.com>
2022-07-09 14:40:33 -0400

    Removed bodyParser from server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require('express');
 require('./db');
 require('./utils/scrape');
-const bodyParser = require('body-parser');
 
-const app = express();
-
-// Parse results 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Express app listening on port 8000
-// app.listen(PORT, () => console.log(`The server is running on PORT: ${PORT}`));
 
