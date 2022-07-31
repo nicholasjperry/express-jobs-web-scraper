6822794b3be0effc6c12acfade0339c7ece7f973
Nick Perry <nicholasjperry1@gmail.com>
2022-07-31 11:44:00 -0400

    Refactored server.js and db.js

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const jobRoutes = require('./routes/jobRoutes');
-const userRoutes = require('./routes/userRoutes');
-const route = require('./routes/route');
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -14,16 +10,7 @@ mongoose
             useNewUrlParser: true,
             useUnifiedTopology: true
         }
-    )
-    .then(() => {
-        const app = express();
-        const PORT = 8000;
-        app.use(express.json());
-        app.use('/api', jobRoutes);
-        app.use('/', userRoutes, route);
-
-        app.listen(PORT, () => console.log(`The server is running on PORT: ${PORT}`));
-    });
+    );
 // Store connection in variable db and log successful connection to console
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const jobRoutes = require('./routes/jobRoutes');
+const userRoutes = require('./routes/userRoutes');
+const route = require('./routes/route');
+
 require('./db');
 const { scrape } = require('./utils/scrape');
-
 scrape();
+
+app.use(express.json());
+app.use('/api', jobRoutes);
+app.use('/', userRoutes, route);
+
+
+app.listen(PORT, () => console.log(`The server is running on PORT: ${PORT}`));
